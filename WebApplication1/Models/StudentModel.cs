20ec7900a40a54b6650f86625faefbdbc359c43a
shxbby <khansgaban@gmail.com>
2024-07-12 08:23:20 +0530

    first commit

---
+++ WebApplication1/Models/StudentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentApplication.Models
+{
+    public class StudentModel
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string city { get; set; }
+
+
+    }
+}
